ca31fea485e36cdb50ee83f548097c69dc48115b
letelumiere <letelumiere@gmail.com>
2023-08-18 13:18:31 +0900

    first commit

---
+++ src/main/java/com/example/dockerjava/Sample.java
@@ -0,0 +1,14 @@
+package com.example.dockerjava;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Sample {
+
+    @RequestMapping("/sample")
+    public String greeting(){
+        return "sample!!";
+    }
+
+}
